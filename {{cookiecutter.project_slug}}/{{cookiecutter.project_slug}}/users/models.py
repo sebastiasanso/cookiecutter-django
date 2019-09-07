14babd5814e1866c674af6ff6b5700995a6f08e1
Sebastià Sansó <sanso.barcelo94@hotmail.com>
2019-09-07 17:30:02 +0200

    Added email as username

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/users/models.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/users/models.py
@@ -2,13 +2,20 @@ from django.contrib.auth.models import AbstractUser
 from django.db.models import CharField
 from django.urls import reverse
 from django.utils.translation import ugettext_lazy as _
+from .managers import CustomUserManager
 
 
 class User(AbstractUser):
+    username = None
+    email = models.EmailField(_('email'), unique=True)
 
-    # First Name and Last Name do not cover name patterns
-    # around the globe.
-    name = CharField(_("Name of User"), blank=True, max_length=255)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name']
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
 
     def get_absolute_url(self):
         return reverse("users:detail", kwargs={"username": self.username})
