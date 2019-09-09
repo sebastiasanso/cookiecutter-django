de5979a8fe122236aee40b116ebc88555727322e
Sebastià Sansó <sebastian.sanso@TOOLFACTORY.NET>
2019-09-09 10:32:20 +0200

    Fix errors

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/users/models.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/users/models.py
@@ -2,13 +2,13 @@ from django.contrib.auth.models import AbstractUser
 from django.db.models import CharField
 from django.urls import reverse
 from django.utils.translation import ugettext_lazy as _
-from .managers import CustomUserManager
+from .managers import UserManager
 
 
 class User(AbstractUser):
     username = None
 
-    objects = CustomUserManager()
+    objects = UserManager()
 
     def get_absolute_url(self):
         return reverse("users:detail", kwargs={"email": self.email})
