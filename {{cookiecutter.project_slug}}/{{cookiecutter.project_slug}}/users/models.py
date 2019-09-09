410050f7bcd260832be71109787a8baa859c0a39
Sebastià Sansó <sebastian.sanso@TOOLFACTORY.NET>
2019-09-09 11:00:09 +0200

    Final version of username

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/users/models.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/users/models.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.models import AbstractUser
-from django.db.models import CharField
+from django.db.models import EmailField
 from django.urls import reverse
 from django.utils.translation import ugettext_lazy as _
 from .managers import UserManager
