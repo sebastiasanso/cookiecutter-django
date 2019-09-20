a3fd803e0ed6db0d67c18ded5e2138423d0bce00
Sebastià Sansó <sanso.barcelo94@hotmail.com>
2019-09-20 13:00:31 +0200

    Fix users urls

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/users/urls.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/users/urls.py
@@ -9,8 +9,8 @@ from {{cookiecutter.project_slug}}.users.views import (
 
 app_name = "users"
 urlpatterns = [
-    path("~redirect/", view=user_redirect_view, name="redirect"),
-    path("~update/", view=user_update_view, name="update"),
-    path("/", view=current_user_detail_view, name="detail"),
+    path("redirect/", view=user_redirect_view, name="redirect"),
+    path("update/", view=user_update_view, name="update"),
+    path("", view=current_user_detail_view, name="current_user_detail"),
     path("<str:email>/", view=user_detail_view, name="detail"),
 ]
